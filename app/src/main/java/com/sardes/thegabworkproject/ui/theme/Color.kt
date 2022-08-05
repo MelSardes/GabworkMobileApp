7cc7df53fb51ac75a3068e148f275c8c74c69b11
Mel <melsardes2042@hotmail.com>
2022-08-05 05:46:33 +0100

    Modify brown color

--- app/src/main/java/com/sardes/thegabworkproject/ui/theme/Color.kt
+++ app/src/main/java/com/sardes/thegabworkproject/ui/theme/Color.kt
@@ -7,7 +7,7 @@ val Purple500 = Color(0xFF6200EE)
 val Purple700 = Color(0xFF3700B3)
 val Teal200 = Color(0xFF03DAC5)
 
-val SmoothBrown = Color(0xFF3F2929)
+val SmoothBrown = Color(0xFF897C7C)
 val GreenFlag = Color(0xFF00995D)
 val YellowFlag = Color(0xFFF4CA15)
 val BlueFlag = Color(0xFF3469B1)
