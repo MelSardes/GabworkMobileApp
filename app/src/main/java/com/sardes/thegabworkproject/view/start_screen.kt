4cf183900a5cb45de3ac67739947211e93fbc1cc
Mel <melsardes2042@hotmail.com>
2022-08-05 05:45:21 +0100

    End write the get started screen

--- app/src/main/java/com/sardes/thegabworkproject/view/start_screen.kt
+++ app/src/main/java/com/sardes/thegabworkproject/view/start_screen.kt
@@ -1,69 +1,138 @@
-package com.sardes.thegabworkproject.view
+package com.sardes.thegabworkproject
 
+import androidx.compose.foundation.Image
 import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.*
 import androidx.compose.foundation.shape.RoundedCornerShape
-import androidx.compose.material.MaterialTheme
-import androidx.compose.material3.Text
+import androidx.compose.material.Button
+import androidx.compose.material.ButtonDefaults
+import androidx.compose.material.Icon
+import androidx.compose.material.Text
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.ArrowForward
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.clip
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.text.SpanStyle
+import androidx.compose.ui.text.buildAnnotatedString
+import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.text.style.TextAlign
+import androidx.compose.ui.text.withStyle
+import androidx.compose.ui.tooling.preview.Devices
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
-import com.sardes.thegabworkproject.ui.theme.SmoothBrown
-import com.sardes.thegabworkproject.ui.theme.YellowFlag
+import androidx.compose.ui.unit.sp
+import com.sardes.thegabworkproject.ui.theme.*
 
 @Composable
-fun Start_Screen() {
-    Column {
-        Box(
+fun Get_Started_Screen() {
+    Box(modifier = Modifier
+        .fillMaxWidth()
+        .fillMaxHeight(0.8f),
+    ){
+        Column (
+            verticalArrangement = Arrangement.SpaceAround,
+            horizontalAlignment = Alignment.CenterHorizontally,
             modifier = Modifier
-                .padding(5.dp, 10.dp, 5.dp, 0.dp)
-                .clip(RoundedCornerShape(30.dp))
-                .background(SmoothBrown)
                 .fillMaxWidth()
-                .fillMaxHeight(0.5f)
+                .fillMaxHeight()
         ){
-/*            Image(
-                painter = painterResource(id = R.drawable.gabwork_on_start_illustration),
-                contentDescription = "Gabwork on start illustration"
-            )*/
-
-
+            Get_Started_top_box()
+            Get_Started_Text()
+            Button_Get_Started()
         }
     }
 }
 
+@Composable
+fun Get_Started_top_box() {
+    Box(
+        modifier = Modifier
+            .padding(5.dp, 10.dp, 5.dp, 0.dp)
+            .clip(RoundedCornerShape(30.dp))
+            .fillMaxWidth()
+            .fillMaxHeight(0.5f)
+            .background(SmoothBrown),
+        contentAlignment = Alignment.Center,
+    ){
+        Image(
+            painter = painterResource(id = R.drawable.gabwork_on_start_illustration_medium),
+            contentDescription = "Gabwork on start illustration"
+        )
+    }
+}
 
 @Composable
-fun Entry_Text(){
+fun Get_Started_Text(){
     Column(horizontalAlignment = Alignment.CenterHorizontally) {
         Text(
-            text = "Devenez Gabworker",
-            color = YellowFlag,
-            style = MaterialTheme.typography.h1
+            buildAnnotatedString {
+                append("Devenez ")
+                withStyle(style = SpanStyle(color = GreenFlag)){
+                    append("G")
+                }
+                withStyle(style = SpanStyle(color = YellowFlag)){
+                    append("a")
+                }
+                withStyle(style = SpanStyle(color = BlueFlag)){
+                    append("b")
+                }
+                append("worker")
+            },
+            fontFamily = yanone,
+            fontWeight = FontWeight.SemiBold,
+            fontSize = 50.sp,
+            textAlign = TextAlign.Center,
+            modifier = Modifier
+                .fillMaxWidth(1f)
             )
+
         Text(text = "Un lieu pour les entreprises, \n" +
                 "les chercheurs d’emploie, \n" +
                 "les étudiants,\n" +
                 "les travailleurs indépendants\n" +
-                "et tout le monde ")
+                "et tout le monde ",
+            fontFamily = yanone,
+            fontWeight = FontWeight.Light,
+            fontSize = 24.sp,
+            textAlign = TextAlign.Center,
+            modifier = Modifier
+                .fillMaxWidth(1f)
+        )
     }
 }
 
-@Preview
 @Composable
-fun Preview_Text(){
-    Entry_Text()
+fun Button_Get_Started() {
+    Button(
+        onClick = { /*TODO*/ },
+        colors = ButtonDefaults.buttonColors(backgroundColor = BlueFlag),
+        shape = RoundedCornerShape(30.dp),
+    ) {
+        Text(
+            "Commencer",
+            color = YellowFlag,
+            textAlign = TextAlign.Center,
+            fontFamily = yanone,
+            fontWeight = FontWeight.Medium
+        )
+        Spacer(Modifier.size(ButtonDefaults.IconSpacing))
+        Icon(
+            Icons.Filled.ArrowForward,
+            tint = YellowFlag,
+            contentDescription = "Arrow",
+            modifier = Modifier.size(ButtonDefaults.IconSize)
+        )
+    }
 }
-
-/*
+@Preview
 @Preview(showSystemUi = true, showBackground = true, device = Devices.NEXUS_6)
 @Preview(showSystemUi = true, showBackground = true, device = Devices.PIXEL)
 @Preview(showSystemUi = true, showBackground = true, device = Devices.PHONE)
 @Preview(showSystemUi = true, showBackground = true, device = Devices.PIXEL_3A)
 @Composable
-fun Preview_screen(){
-    Start_Screen()
-}*/
+fun Preview_Text(){
+    Get_Started_Screen()
+}
