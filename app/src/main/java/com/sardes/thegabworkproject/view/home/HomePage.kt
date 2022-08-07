dc065057d8d139dae354158e8a4b2056c88b63ba
Mel <melsardes2042@hotmail.com>
2022-08-07 20:14:09 +0100

    Create Home page

---
+++ app/src/main/java/com/sardes/thegabworkproject/view/home/HomePage.kt
@@ -0,0 +1,9 @@
+package com.sardes.thegabworkproject.view.home
+
+import androidx.compose.material.Text
+import androidx.compose.runtime.Composable
+
+@Composable
+fun Home(){
+    Text("W E L C O M E")
+}
