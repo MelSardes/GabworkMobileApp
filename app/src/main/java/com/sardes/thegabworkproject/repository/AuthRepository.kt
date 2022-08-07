e6494f9e27af40b3481b38da3cd6ff506679d5a6
Mel <melsardes2042@hotmail.com>
2022-08-07 20:05:52 +0100

    Add a Firebase Auth repository

---
+++ app/src/main/java/com/sardes/thegabworkproject/repository/AuthRepository.kt
@@ -0,0 +1,50 @@
+package com.sardes.thegabworkproject.repository
+
+import com.google.firebase.auth.FirebaseUser
+import com.google.firebase.auth.ktx.auth
+import com.google.firebase.ktx.Firebase
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.tasks.await
+import kotlinx.coroutines.withContext
+
+class AuthRepository {
+    // TO GET THE CURRENT USER
+    val currentUser: FirebaseUser? = Firebase.auth.currentUser
+
+    fun hasUser(): Boolean = Firebase.auth.currentUser != null
+
+    // GET THE USER ID
+    fun getUserId(): String = Firebase.auth.currentUser?.uid.orEmpty()
+
+    // FUNCTION FOR CREATE A USER
+    suspend fun createUser(
+        email: String,
+        password: String,
+        onComplete:(Boolean) -> Unit
+    ) = withContext(Dispatchers.IO){
+        Firebase.auth
+            .createUserWithEmailAndPassword(email, password)
+            .addOnCompleteListener {
+                if (it.isSuccessful){
+                    onComplete.invoke(true)
+                }else
+                    onComplete.invoke(false)
+            }
+    }.await()
+
+    // FUNCTION FOR LOGIN A USER
+    suspend fun login(
+        email: String,
+        password: String,
+        onComplete:(Boolean) -> Unit
+    ) = withContext(Dispatchers.IO){
+        Firebase.auth
+            .signInWithEmailAndPassword(email, password)
+            .addOnCompleteListener {
+                if (it.isSuccessful){
+                    onComplete.invoke(true)
+                }else
+                    onComplete.invoke(false)
+            }
+    }.await()
+}
