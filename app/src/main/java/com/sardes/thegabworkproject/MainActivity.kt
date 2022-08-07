1565ee63df58780837f5da84c58d5a5bc7a18f04
Mel <melsardes2042@hotmail.com>
2022-08-07 20:22:51 +0100

    Set NavGraph into MainActivity.kt

--- app/src/main/java/com/sardes/thegabworkproject/MainActivity.kt
+++ app/src/main/java/com/sardes/thegabworkproject/MainActivity.kt
@@ -1,20 +1,25 @@
 package com.sardes.thegabworkproject
 
-import android.annotation.SuppressLint
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.lifecycle.viewmodel.compose.viewModel
+import com.sardes.thegabworkproject.navigation.SetupNavGraph
 import com.sardes.thegabworkproject.ui.theme.TheGabworkProjectTheme
+import com.sardes.thegabworkproject.view.login.LoginViewModel
 
 
 class MainActivity : ComponentActivity() {
 
-    @SuppressLint("UnusedMaterialScaffoldPaddingParameter")
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContent {
+            val loginViewModel = viewModel(modelClass = LoginViewModel::class.java)
+
             TheGabworkProjectTheme{
-                // TODO
+
+                SetupNavGraph(loginViewModel = loginViewModel)
+
             }
         }
     }
